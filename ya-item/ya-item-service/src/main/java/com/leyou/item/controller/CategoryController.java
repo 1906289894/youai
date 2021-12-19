77311022c47244191fdd46d8400ea0fcdc29615b
‘fubw’ <894106999@qq.com>
2021-12-19 20:53:18 +0800

    :construction:

--- ya-item/ya-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ ya-item/ya-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -1,9 +1,12 @@
 package com.leyou.item.controller;
 
+import com.leyou.item.dto.CategoryDTO;
 import com.leyou.item.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("category")
@@ -15,4 +18,36 @@ public class CategoryController {
     /*public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }*/
+
+
+    /**
+     * 根据id查询分类
+     */
+
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDTO> queryCategoryById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(new CategoryDTO(categoryService.getById(id)));
+    }
+
+    /**
+     * 根据分类id集合查询分类集合
+     * @param ids
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<CategoryDTO>> queryCategoryByIds(@RequestParam("ids") List<Long> ids){
+        return ResponseEntity.ok(CategoryDTO.convertEntityList(categoryService.listByIds(ids)));
+    }
+
+
+    @GetMapping("/of/parent")
+    public ResponseEntity<List<CategoryDTO>> queryCategoryByParentId(@RequestParam("pid") Long pid){
+        return ResponseEntity.ok(// 封装并返回
+                CategoryDTO.convertEntityList(// 把PO集合转为DTO集合
+                        categoryService.query().eq("parent_id", pid).list()// 根据父类目id查询集合
+                )
+        );
+    }
+
+
 }
